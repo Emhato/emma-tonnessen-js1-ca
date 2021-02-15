9d2d9656c674c9bd5970430ea210daf810ae8124
Emhato <71319235+Emhato@users.noreply.github.com>
2021-02-15 16:20:43 +0100

    started on day 2

--- js/script-1.js
+++ js/script-1.js
@@ -1,43 +1,39 @@
 
 
 
+// Api info: https://docs.trefle.io/docs/guides/getting-started/
 
+// Botanics token: CWjAkNFJA0A2aMCvqHZh5FXHPyzn_ghmbIIc84-fgaA
 
+const url = "https://trefle.io/api/v1/plants?token=CWjAkNFJA0A2aMCvqHZh5FXHPyzn_ghmbIIc84-fgaA";
 
+const cors = "https://noroffcors.herokuapp.com/";
 
-// API info: https://www.songsterr.com/a/wa/api/
-
-const url = "http://www.songsterr.com/a/ra/songs.json?pattern=beatles";
-
-// const cors = "https://noroffcors.herokuapp.com/";
-
-// const corsFix = cors + url;
+const corsFix = cors + url;
 
 const apiContainer = document.querySelector(".api-container");
 
 async function getArchive() {
     try {
-        const response = await fetch(url);
+        const response = await fetch(corsFix);
 
         const data = await response.json();
 
         console.log(data);
 
-        // facts = data.results
+        const facts = data.data
 
-        // apiContainer.innerHTML = "";
+        apiContainer.innerHTML = "";
 
-        // for (let i = 0; i < facts.length; i++) {
+        for (let i = 0; i < facts.length; i++) {
 
-        //     if (i === 8) {
-        //         break;
-        //     }
+            if (i === 4) {
+                break;
+            }
 
-        //     apiContainer.innerHTML += `<div class="result"><p>Name: ${facts[i].name}</p>
-        //     <p>Rating: ${facts[i].rating}</p>
-        //     <p> Number of tagss: ${facts[i].tags.length}</div>`;
-        // }
-
+            apiContainer.innerHTML += `<div class="result"><p>Name: ${facts[i].common_name}</p>
+                                        <p>Family: ${facts[i].family}</p></div>`;
+        }
     } catch (error) {
         console.log(error)
         console.log("An error occurred");
