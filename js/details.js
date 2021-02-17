0aa659c973d52264a724675b6c74de7946924979
Emhato <71319235+Emhato@users.noreply.github.com>
2021-02-17 21:15:04 +0100

    woho!

--- js/details.js
+++ js/details.js
@@ -35,6 +35,102 @@
 
 // getPlant();
 
+// const queryString = document.location.search;
+
+// const params = new URLSearchParams(queryString);
+
+// const nameParam = params.get("scientific_name");
+
+// console.log(nameParam);
+
+
+
+// const url = "https://trefle.io/api/v1/plants?token=CWjAkNFJA0A2aMCvqHZh5FXHPyzn_ghmbIIc84-fgaA";
+
+// console.log(url);
+
+// const cors = "https://noroffcors.herokuapp.com/";
+
+// const corsFix = cors + url;
+
+// const plantContainer = document.querySelector(".plant-container");
+
+// async function getPlant() {
+//     try {
+//         const response = await fetch(corsFix);
+
+//         const data = await response.json();
+
+//         console.log(data);
+
+//         const facts = data.data;
+
+//         plantContainer.innerHTML = "";
+
+//         plantContainer.innerHTML = `<div class="results">
+//                                     <h1>${facts[0].common_name}</h1>
+//                                     <div class="details-image"
+//                                         style="background-image: url('${facts[0].image_url}')"></div>
+//                                     <p>${facts[0].common_name} or ${facts[0].scientific_name} belongs to the ${facts[0].family_common_name} (${facts[0].family})</p>
+//                                     </div>`;
+
+//     } catch (error) {
+//         console.log(error);
+//         console.log("An error occurred");
+//         plantContainer.innerHTML = "An error has occured, sorry for the inconvinience this may cause!";
+//     }
+
+// }
+
+// getPlant();
+
+
+// const queryString = document.location.search;
+
+// const params = new URLSearchParams(queryString);
+
+// const nameParam = params.get("[scientific_name]");
+
+// console.log(nameParam);
+
+
+
+// const url = "https://trefle.io/api/v1/plants?token=CWjAkNFJA0A2aMCvqHZh5FXHPyzn_ghmbIIc84-fgaA&" + nameParam;
+
+// const cors = "https://noroffcors.herokuapp.com/";
+
+// const corsFix = cors + url;
+
+// const plantContainer = document.querySelector(".plant-container");
+
+// async function getPlant() {
+//     try {
+//         const response = await fetch(corsFix);
+
+//         const data = await response.json();
+
+//         console.log(data);
+
+//         const facts = data.data;
+
+//         plantContainer.innerHTML = "";
+
+//         plantContainer.innerHTML = `<div class="results">
+//                                     <h1>${facts[0].common_name}</h1>
+//                                     <div class="details-image"
+//                                         style="background-image: url('${facts[0].image_url}')"></div>
+//                                     <p>${facts[0].common_name} or ${facts[0].scientific_name} belongs to the ${facts[0].family_common_name} (${facts[0].family})</p>
+//                                     </div>`;
+
+//     } catch (error) {
+//         console.log(error);
+//         console.log("An error occurred");
+//         plantContainer.innerHTML = "An error has occured, sorry for the inconvinience this may cause!";
+//     }
+
+// }
+
+
 const queryString = document.location.search;
 
 const params = new URLSearchParams(queryString);
--- js/script-1.js
+++ js/script-1.js
@@ -2,6 +2,49 @@
 
 // // Botanics token: CWjAkNFJA0A2aMCvqHZh5FXHPyzn_ghmbIIc84-fgaA
 
+// const url = "https://trefle.io/api/v1/plants?token=CWjAkNFJA0A2aMCvqHZh5FXHPyzn_ghmbIIc84-fgaA";
+
+// const cors = "https://noroffcors.herokuapp.com/";
+
+// const corsFix = cors + url;
+
+// const apiContainer = document.querySelector(".api-container");
+
+// async function getArchive() {
+//     try {
+//         const response = await fetch(corsFix);
+
+//         const data = await response.json();
+
+//         console.log(data);
+
+//         const facts = data.data;
+
+//         apiContainer.innerHTML = "";
+
+//         for (let i = 0; i < facts.length; i++) {
+
+//             if (i === 4) {
+//                 break;
+//             }
+
+//             apiContainer.innerHTML += `<a href="details.html?filter[scientific_name]=${facts[i].scientific_name}" class="result"><h2>${facts[i].common_name}</h2>
+//                                         <p>(${facts[i].scientific_name})</p>
+//                                         <p>In the ${facts[i].family_common_name}</p>
+//                                         <p>(${facts[i].family})</p>
+//                                         <p>(${facts[i].links.plant})</p>
+//                                         <p>(${facts[i].slug})</p></a>`;
+//         }
+//     } catch (error) {
+//         console.log(error);
+//         console.log("An error occurred");
+//         apiContainer.innerHTML = "An error has occured, sorry for the inconvinience this may cause!";
+//     }
+
+// }
+
+// getArchive();
+
 const url = "https://trefle.io/api/v1/plants?token=CWjAkNFJA0A2aMCvqHZh5FXHPyzn_ghmbIIc84-fgaA";
 
 const cors = "https://noroffcors.herokuapp.com/";
@@ -28,7 +71,7 @@ async function getArchive() {
                 break;
             }
 
-            apiContainer.innerHTML += `<a href="details.html&filter[scientific_name]=${facts[i].scientific_name}" class="result"><h2>${facts[i].common_name}</h2>
+            apiContainer.innerHTML += `<a href="details.html?filter[scientific_name]=${facts[i].scientific_name}" class="result"><h2>${facts[i].common_name}</h2>
                                         <p>(${facts[i].scientific_name})</p>
                                         <p>In the ${facts[i].family_common_name}</p>
                                         <p>(${facts[i].family})</p>
